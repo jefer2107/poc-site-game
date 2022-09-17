f8708cb2d512576038aa8cd4f224b00bd7b39870
Jefferson Dias <jefer.ld@hotmail.com>
2022-09-17 16:45:16 -0300

    adjustment

--- src/home.js
+++ src/home.js
@@ -3,15 +3,15 @@ import Unity, {UnityContext} from 'react-unity-webgl';
 export const Home = () => {
 
     const unityContext = new UnityContext({
-        loaderUrl: '/build/CassinoWebGl.loader.js',
-        dataUrl: '/build/webgl.data',
-        frameworkUrl: '/build/build.framework.js',
-        codeUrl: '/build/build.wasm',
+        loaderUrl: '/roleta/BuildTestLink.loader.js',
+        dataUrl: '/roleta/webgl.data',
+        frameworkUrl: '/roleta/build.framework.js',
+        codeUrl: '/roleta/build.wasm',
       });
 
     return(
         <div>
-            <Unity  unityContext={unityContext} style={{width: "100%", height: "100%", justifySelf: "center", alignSelf: "center"}} />
+            <Unity  unityContext={unityContext} style={{width: "390px", height: "844px", justifySelf: "center", alignSelf: "center"}} />
         </div>
     )
 }
